d3f33f82309748df4823459add992bbfd50b4333
Suraj021992 <shelakesuraj02@gmail.com>
2020-08-19 19:54:06 +0530

    New TestNG class

--- src/test/java/testcases/Test2.java
+++ src/test/java/testcases/Test2.java
@@ -1,11 +1,24 @@
 package testcases;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Test2 {
+	@BeforeMethod
+	public void Before()
+	{
+		System.out.println("Befor Method");
+	}
   @Test
   public void f() 
   {
 	  System.out.println("In a Test 2 class");
+  }
+  
+  @AfterMethod
+  public void after()
+  {
+	  System.out.println("After Method");
   }
 }
