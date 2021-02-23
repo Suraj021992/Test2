4726378b0edde4fb958e35d890dbfdaf910cea29
Suraj021992 <shelakesuraj02@gmail.com>
2021-02-23 21:23:30 +0530

    New_webdriver

--- src/test/java/testcases/Test1.java
+++ src/test/java/testcases/Test1.java
@@ -1,8 +1,13 @@
 package testcases;
 
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import appscreen.HomePage_eclipse_download;
+import appscreen.eclipse_releases;
+import appscreen.package_page;
 
 import utility.Launch_browser;
 import utility.ReadProperties;
@@ -10,6 +15,9 @@ import utility.ReadProperties;
 public class Test1 {
 	
 	Launch_browser br=null;
+	HomePage_eclipse_download hp=null;
+	package_page pkg=null;
+	eclipse_releases release=null;
 	
 	@BeforeClass
 	public void launchBrowser()
@@ -21,19 +29,21 @@ public class Test1 {
 	@Test
 	  public void f() 
 	  {
-		  System.out.println(ReadProperties.chrome_browser_path);
-		  System.out.println("In a Test 1");
-		  System.out.println("Temp");
+		 hp=PageFactory.initElements(Launch_browser.instanceDriver(),HomePage_eclipse_download.class);
+		 hp.click_download_package();
+		
+		 pkg=PageFactory.initElements(Launch_browser.instanceDriver(),package_page.class);
+		 pkg.click_packages();
+		 
+		 release=PageFactory.initElements(Launch_browser.instanceDriver(),eclipse_releases.class);
+		 release.verify_releases();
+		  
 	  }
-	  @Test
-	  public void test2()
-	  {
-		  System.out.println("In a test 2");
-	  }
+	  
 	  
 	  @ AfterClass
 	  public void afterClass()
 	  {
-		  Launch_browser.closeDriver();
+		  //Launch_browser.closeDriver();
 	  }
 }
